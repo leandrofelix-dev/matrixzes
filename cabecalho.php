cd075e729ed627f7852b1ad3ba77e2e673218ff4
lehinfo.felix <lehinfo.felix@gmail.com>
2020-04-04 12:13:52 -0300

    correção de bugs nas cores

--- cabecalho.php
+++ cabecalho.php
@@ -1,4 +1,5 @@
         
+        <link rel="stylesheet" href="css/var.css">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/<?php echo($modo);?>.css">
         <style> td { background: var(--color<?php echo rand(1,15);?>) } </style>
